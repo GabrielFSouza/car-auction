5045e6a6cc173397778eb4bfbe9f1c85bc7d50a4
GabrielFSouza <gabrielferreira717@outlook.com>
2024-09-11 15:22:34 -0300

    car#004 - Colocando o host no server pra ver se está certo

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app'
 import { env } from './core/env'
 
 app.listen(env.PORT, env.HOST, () =>
-  console.log('🚀 Server is running on port ' + env.PORT),
+  console.log('🚀 Server is running on port: ' + env.PORT + ' for the host: ' + env.HOST),
 )
